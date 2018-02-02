c30210b70b724a70e4fc9768e07a1093783d7ffd
Thomas Pollinger <thomas.pollinger@vodafone.com>
2018-02-02 09:32:30 +0100

    Upgrade Highlight to WSM-Level ;)

---
+++ shBrushWSM.js
@@ -0,0 +1,21 @@
+SyntaxHighlighter.brushes.Custom = function () {
+    this.regexList = [
+        { regex: /&lt;%([^!])+?%&gt;/gm, css: 'placeholder' },
+        { regex: /&lt;!(\/?)IoRange(.+?)&gt;/gm, css: 'blockmark' },
+        { regex: /&lt;!IoRedDot(.+?)&gt;/gm, css: 'smartedit-reddot' },
+        { regex: /&lt;%!!(.+?)!!%&gt;/gm, css: 'rendertag-sn' },
+        { regex: /&lt;(\/?)reddot:cms&gt;/gm, css: 'rendertag' },
+        { regex: /&lt;navigation(.+?)&gt;/gm, css: 'rendertag' },
+        { regex: /&lt;(\/?)output(.+?)&gt;/gm, css: 'rendertag' },
+        { regex: /&lt;(\/?)foreach(.+?)&gt;/gm, css: 'rendertag' },
+        { regex: /&lt;(\/?)if&gt;/gm, css: 'rendertag' },
+        { regex: /&lt;query(.+?)&gt;/gm, css: 'rendertag' },
+        { regex: /&lt;(\/?)query&gt;/gm, css: 'rendertag' },
+        { regex: /&lt;(\/?)htmltext&gt;/gm, css: 'rendertag' },
+        { regex: /&lt;(\/?)xmltext&gt;/gm, css: 'rendertag' },
+        { regex: /&lt;(\/?)currentPageCommentCount&gt;/gm, css: 'rendertag' },
+        { regex: /&lt;(\/?)pagesMostCommented&gt;/gm, css: 'rendertag' }
+    ];
+};
+SyntaxHighlighter.brushes.Custom.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Custom.aliases = ['RedDot'];
